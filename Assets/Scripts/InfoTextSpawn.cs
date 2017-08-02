24bc8bcf77079f733d6f19494f72a2ba13f018c1
AkashGutha <akash.gutha@outlook.coma>
2017-08-03 00:50:03 +0530

    first prototype

---
+++ Assets/Scripts/InfoTextSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoTextSpawn : MonoBehaviour
+{
+
+    public GameObject infoText;
+    public Canvas mainCanvas;
+    public GameObject Anchor;
+
+    public float YOffset;
+    private GameObject info;
+    // Use this for initialization
+    void Start()
+    {
+        info = GameObject.Instantiate(infoText);
+        info.transform.SetParent(mainCanvas.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(this.transform.position + new Vector3(0, Anchor.transform.localScale.y/2+YOffset,0 ) );
+        info.transform.position = screenPos;
+
+    }
+}
---
+++ Assets/Scripts/RotateAtSpeed.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAtSpeed : MonoBehaviour {
+ public float speed = 1f;
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, speed, 0);
+    }
+}
