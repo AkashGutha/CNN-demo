b6e3056c8953410488cac9cdac19af51b0091d83
Akash <akash.gutha@outlook.com>
2017-08-04 18:10:27 +0530

    added materails

---
+++ Assets/Scripts/MaterialBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MaterialBuilder : MonoBehaviour
+{
+
+    // make a materials array
+    private Material[] mats = new Material[50];
+
+
+
+    void Awake()
+    {
+        // get the strings and make materials for each state
+        var states = StatesService.states_names;
+        var abbs = StatesService.states_abbrevation;
+
+        for (int i = 0; i < states.Length; i++)
+        {
+            var mat = new Material(Shader.Find("Standard"));
+            mat.name = "MT_" + abbs[i] + "_" + states[i];
+			 AssetDatabase.CreateAsset(mat, "Assets/Materials/States/" + mat.name + ".mat");
+        }
+
+    }
+}
