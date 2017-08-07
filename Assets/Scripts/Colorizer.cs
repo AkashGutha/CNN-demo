7d4cebe63b486dfa17e084e59a15534aa35a08f0
Akash <akash.gutha@outlook.com>
2017-08-07 17:56:50 +0530

    animations

--- Assets/Scripts/Colorizer.cs
+++ Assets/Scripts/Colorizer.cs
@@ -2,33 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Colorizer : MonoBehaviour
 {
-    public Dictionary<string, Transform> States = new Dictionary<string, Transform>();
-  
-    void Awake()
+  public Dictionary<string, Transform> States = new Dictionary<string, Transform>();
+  public Color[] UnemploymentDataColor;
+  public Color UnemploymentDataBaseColor;
+  void Awake()
+  {
+    var states = this.transform.GetComponentsInChildren<Transform>();
+    foreach (var state in states)
     {
-        var states = this.transform.GetComponentsInChildren<Transform>();
-        foreach (var state in states)
-        {
-            if (state == this.transform) continue;
-            this.States.Add(state.name.Trim(), state);
-            // Debug.Log(state.name.Trim());
-        }
-
-        // just for debugging
-        ColorizeByUnemployment();
+      if (state == this.transform) continue;
+      this.States.Add(state.name.Trim(), state);
+      // Debug.Log(state.name.Trim());
     }
-    public void ColorizeByUnemployment()
+
+    // just for debugging
+    ColorizeByUnemployment();
+  }
+  public void ColorizeByUnemployment()
+  {
+    var dataList = UnemploymentDataService.UnemploymentDataList;
+
+    foreach (var state in dataList)
     {
-        var dataList = UnemploymentDataService.UnemploymentDataList;
+      // Debug.Log(state.Name);
+      float strength = ((float)state.UnemploymentDecile / 10);
 
-        foreach (var state in dataList)
-        {
-            // Debug.Log(state.Name);
-            float strength = ((float)state.UnemploymentDecile / 5);
-            States[state.Name].GetComponent<Renderer>().material.color = new Color(.8f, .8f , 0.45f * strength);
-        }
+      States[state.Name].GetComponent<Renderer>().sharedMaterial.color = UnemploymentDataColor[state.UnemploymentDecile - 1];
+
+      //   States[state.Name].GetComponent<Renderer>().sharedMaterial.color = UnemploymentDataBaseColor * strength;
     }
+  }
+
+  void Update()
+  {
+      ColorizeByUnemployment();
+  }
 
 }
