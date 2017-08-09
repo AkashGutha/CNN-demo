92375fbe9992fa58dafd79a7d711c85e73b6d133
Akash <akash.gutha@outlook.com>
2017-08-09 17:43:40 +0530

    added changes

--- Assets/Scripts/statenamer.cs
+++ Assets/Scripts/statenamer.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class statenamer : MonoBehaviour
 {
 
-    void Awake()
+  //   void Awake()
+  //   {
+  //     var state_names = StatesService.states_names;
+  //     var state_abbs = StatesService.states_abbrevation;
+
+  //     var children = gameObject.GetComponentsInChildren<Transform>();
+  //     for (int i = 0; i < children.Length - 1; i++)
+  //     {
+  //       // if (children[i] == this.transform) continue;
+  //       children[i + 1].name = state_names[i];
+
+  //       children[i + 1].GetComponent<Renderer>().material = Resources.Load("States_Materials/MT_" + state_abbs[i] + "_" + state_names[i], typeof(Material)) as Material; ;
+  //     }
+  //   }
+
+  void Start()
+  {
+    var counties = CountiesService.CountiesList;
+
+    foreach (var county in counties)
     {
-        var state_names = StatesService.states_names;
-        var state_abbs = StatesService.states_abbrevation;
-
-        var children = gameObject.GetComponentsInChildren<Transform>();
-        for (int i = 0; i < children.Length-1; i++)
-        {
-            // if (children[i] == this.transform) continue;
-            children[i+1].name = state_names[i];
-
-            children[i+1].GetComponent<Renderer>().material = Resources.Load("States_Materials/MT_" + state_abbs[i] + "_" + state_names[i], typeof(Material)) as Material; ;
-        }
+      Debug.Log(county);
     }
+  }
 }
